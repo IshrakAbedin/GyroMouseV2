e048fb118efc8ec32de2d2d1e144e29511e53729
Rizvi-Ahmed <rizvi.ahmed1020@gmil.com>
2019-10-13 00:48:52 +0600

    Code Formatted and functions added

--- Filter/MPU6050withfilter.ino
+++ Filter/MPU6050withfilter.ino
@@ -1,8 +1,10 @@
 #include <Wire.h>
 #include "MPU6050lib.h"
-#define blinkPin 13 // Blink LED on Teensy or Pro Mini when updating   
+#define blinkPin 13 // Blink LED on Teensy or Pro Mini when updating
 #define freq 10
 MPU6050lib mpu;
+
+
 
 float aRes, gRes; // scale resolutions per LSB for the sensors
 // Pin definitions
@@ -14,12 +16,15 @@ int16_t gyroCount[3];                                    // Stores the 16-bit si
 float gx, gy, gz;                                        // Stores the real gyro value in degrees per seconds
 float gyroBias[3] = {0, 0, 0}, accelBias[3] = {0, 0, 0}; // Bias corrections for gyro and accelerometer
 int16_t tempCount;                                       // Stores the real internal chip temperature in degrees Celsius
-float temperature;
+//float temperature;
 float SelfTest[6];
 float q[4] = {1.0f, 0.0f, 0.0f, 0.0f}; // vector to hold quaternion
 uint32_t delt_t = 0;                   // used to control display output rate
 uint32_t count = 0;                    // used to control display output rate
 float pitch, yaw, roll;
+
+
+//Madgwick variables Start
 
 float GyroMeasError = PI * (40.0f / 180.0f);    // gyroscope measurement error in rads/s (start at 60 deg/s), then reduce after ~10 s to 3
 float beta = sqrt(3.0f / 4.0f) * GyroMeasError; // compute beta
@@ -29,178 +34,61 @@ float deltat = 0.0f;                            // integration interval for both
 uint32_t lastUpdate = 0, firstUpdate = 0;
 uint32_t Now = 0;
 
+float q1, q2, q3, q4;                                     // short name local variable for readability
+float norm;                                               // vector norm
+float f1, f2, f3;                                         // objective funcyion elements
+float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33; // objective function Jacobian elements
+float qDot1, qDot2, qDot3, qDot4;
+float hatDot1, hatDot2, hatDot3, hatDot4;
+float gerrx, gerry, gerrz, gbiasx, gbiasy, gbiasz; // gyro bias error
+
+// Auxiliary variables to avoid repeated arithmetic
+float _halfq1;
+float _halfq2;
+float _halfq3;
+float _halfq4;
+float _2q1;
+float _2q2;
+float _2q3;
+float _2q4;
+float _2q1q3;
+float _2q3q4;
+
+// Madgwick Variable Ends
+
 void setup()
 {
   Wire.begin();
   Wire.setClock(400000);
   Serial.begin(115200);
-
-  // Set up the interrupt pin, its set as active high, push-pull
-  //pinMode(intPin, INPUT);
-  //digitalWrite(intPin, LOW);
-  pinMode(blinkPin, OUTPUT);
-  digitalWrite(blinkPin, HIGH);
-
-  // Read the WHO_AM_I register, this is a good test of communication
-  uint8_t c = mpu.readByte(MPU6050_ADDRESS, WHO_AM_I_MPU6050); // Read WHO_AM_I register for MPU-6050
-  Serial.print("I AM ");
-  Serial.print(c, HEX);
-  Serial.print(" I Should Be ");
-  Serial.println(0x68, HEX);
-
-  if (c == 0x68) // WHO_AM_I should always be 0x68
-  {
-    Serial.println("MPU6050 is online...");
-
-    mpu.MPU6050SelfTest(SelfTest); // Start by performing self test and reporting values
-    //    Serial.print("x-axis self test: acceleration trim within : "); Serial.print(SelfTest[0],1); Serial.println("% of factory value");
-    //    Serial.print("y-axis self test: acceleration trim within : "); Serial.print(SelfTest[1],1); Serial.println("% of factory value");
-    //    Serial.print("z-axis self test: acceleration trim within : "); Serial.print(SelfTest[2],1); Serial.println("% of factory value");
-    //    Serial.print("x-axis self test: gyration trim within : "); Serial.print(SelfTest[3],1); Serial.println("% of factory value");
-    //    Serial.print("y-axis self test: gyration trim within : "); Serial.print(SelfTest[4],1); Serial.println("% of factory value");
-    //    Serial.print("z-axis self test: gyration trim within : "); Serial.print(SelfTest[5],1); Serial.println("% of factory value");
-
-    if (SelfTest[0] < 1.0f && SelfTest[1] < 1.0f && SelfTest[2] < 1.0f && SelfTest[3] < 1.0f && SelfTest[4] < 1.0f && SelfTest[5] < 1.0f)
-    {
-      Serial.println("Pass Selftest!");
-
-      mpu.calibrateMPU6050(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
-      Serial.println("MPU6050 bias");
-      Serial.println(" x\t  y\t  z  ");
-      Serial.print((int)(1000 * accelBias[0]));
-      Serial.print('\t');
-      Serial.print((int)(1000 * accelBias[1]));
-      Serial.print('\t');
-      Serial.print((int)(1000 * accelBias[2]));
-      Serial.println(" mg");
-
-      Serial.print(gyroBias[0], 1);
-      Serial.print('\t');
-      Serial.print(gyroBias[1], 1);
-      Serial.print('\t');
-      Serial.print(gyroBias[2], 1);
-      Serial.println(" o/s");
-
-      mpu.initMPU6050();
-      Serial.println("MPU6050 initialized for active data mode...."); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
-    }
-    else
-    {
-      Serial.print("Could not connect to MPU6050: 0x");
-      Serial.println(c, HEX);
-      while (1)
-        ; // Loop forever if communication doesn't happen
-    }
-  }
+  myinit();
 }
 
 void loop()
 {
-  // If data ready bit set, all data registers have new data
-  if (mpu.readByte(MPU6050_ADDRESS, INT_STATUS) & 0x01)
-  {                                // check if data ready interrupt
-    mpu.readAccelData(accelCount); // Read the x/y/z adc values
-    aRes = mpu.getAres();
+  getReadings();
 
-    // Now we'll calculate the accleration value into actual g's
-    ax = (float)accelCount[0] * aRes; // get actual g value, this depends on scale being set
-    ay = (float)accelCount[1] * aRes;
-    az = (float)accelCount[2] * aRes;
-
-    //Serial.print(ax);Serial.print("\t");
-    //Serial.print(ay);Serial.print("\t");
-    //Serial.print(az);Serial.print("\t");
-
-    mpu.readGyroData(gyroCount); // Read the x/y/z adc values
-    gRes = mpu.getGres();
-
-    // Calculate the gyro value into actual degrees per second
-    gx = (float)gyroCount[0] * gRes; // get actual gyro value, this depends on scale being set
-    gy = (float)gyroCount[1] * gRes;
-    gz = (float)gyroCount[2] * gRes;
-
-    //Serial.print(gx);Serial.print("\t");
-    //Serial.print(gy);Serial.print("\t");
-    //Serial.println(gz);
-
-    //tempCount = mpu.readTempData();  // Read the x/y/z adc values
-    //temperature = ((float) tempCount) / 340. + 36.53; // Temperature in degrees Centigrade
-  }
-
-  Now = micros();
-  deltat = ((Now - lastUpdate) / 1000000.0f); // set integration time by time elapsed since last filter update
-  lastUpdate = Now;
-
-  MadgwickQuaternionUpdate(ax, ay, az, gx * PI / 180.0f, gy * PI / 180.0f, gz * PI / 180.0f);
-
-  // Serial print and/or display at 0.5 s rate independent of data rates
-  delt_t = millis() - count;
-  if (delt_t > freq)
-  { // update LCD once per half-second independent of read rate
-    digitalWrite(blinkPin, blinkOn);
-
-    yaw = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);
-    pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
-    roll = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
-
-    pitch *= 180.0f / PI;
-    yaw *= 180.0f / PI;
-    roll *= 180.0f / PI;
-
-    Serial.print("Yaw, Pitch, Roll: \t");
-    Serial.print(yaw, 2);
-    Serial.print(", ");
-    Serial.print(pitch, 2);
-    Serial.print(", ");
-    Serial.println(roll, 2);
-
-    //    Serial.print("average rate = "); Serial.print(1.0f/deltat, 2); Serial.println(" Hz");
-
-    /*Serial.println(" x\t  y\t  z  ");
-
-    Serial.print((int)(1000 * ax)); Serial.print('\t');
-    Serial.print((int)(1000 * ay)); Serial.print('\t');
-    Serial.print((int)(1000 * az));
-    Serial.println(" mg");
-
-    Serial.print((int)(gx)); Serial.print('\t');
-    Serial.print((int)(gy)); Serial.print('\t');
-    Serial.print((int)(gz));
-    Serial.println(" o/s");
-
-    Serial.print((int)(yaw)); Serial.print('\t');
-    Serial.print((int)(pitch)); Serial.print('\t');
-    Serial.print((int)(roll));
-    Serial.println(" ypr");*/
-
-    //Serial.print("rt: "); Serial.print(1.0f / deltat, 2); Serial.println(" Hz");
-
-    blinkOn = ~blinkOn;
-    count = millis();
-  }
+  Serial.print("Pitch, Roll: \t");
+  Serial.print(pitch, 2);
+  Serial.print(", ");
+  Serial.println(roll, 2);
 }
 
 void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz)
 {
-  float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];         // short name local variable for readability
-  float norm;                                               // vector norm
-  float f1, f2, f3;                                         // objective funcyion elements
-  float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33; // objective function Jacobian elements
-  float qDot1, qDot2, qDot3, qDot4;
-  float hatDot1, hatDot2, hatDot3, hatDot4;
-  float gerrx, gerry, gerrz, gbiasx, gbiasy, gbiasz; // gyro bias error
+  q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3]; // short name local variable for readability
 
   // Auxiliary variables to avoid repeated arithmetic
-  float _halfq1 = 0.5f * q1;
-  float _halfq2 = 0.5f * q2;
-  float _halfq3 = 0.5f * q3;
-  float _halfq4 = 0.5f * q4;
-  float _2q1 = 2.0f * q1;
-  float _2q2 = 2.0f * q2;
-  float _2q3 = 2.0f * q3;
-  float _2q4 = 2.0f * q4;
-  float _2q1q3 = 2.0f * q1 * q3;
-  float _2q3q4 = 2.0f * q3 * q4;
+  _halfq1 = 0.5f * q1;
+  _halfq2 = 0.5f * q2;
+  _halfq3 = 0.5f * q3;
+  _halfq4 = 0.5f * q4;
+  _2q1 = 2.0f * q1;
+  _2q2 = 2.0f * q2;
+  _2q3 = 2.0f * q3;
+  _2q4 = 2.0f * q4;
+  _2q1q3 = 2.0f * q1 * q3;
+  _2q3q4 = 2.0f * q3 * q4;
 
   // Normalise accelerometer measurement
   norm = sqrt(ax * ax + ay * ay + az * az);
@@ -269,3 +157,142 @@ void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, 
   q[3] = q4 * norm;
 }
 
+void myinit()
+{
+  // Set up the interrupt pin, its set as active high, push-pull
+  //pinMode(intPin, INPUT);
+  //digitalWrite(intPin, LOW);
+  pinMode(blinkPin, OUTPUT);
+  digitalWrite(blinkPin, HIGH);
+
+  // Read the WHO_AM_I register, this is a good test of communication
+  uint8_t c = mpu.readByte(MPU6050_ADDRESS, WHO_AM_I_MPU6050); // Read WHO_AM_I register for MPU-6050
+  Serial.print("I AM ");
+  Serial.print(c, HEX);
+  Serial.print(" I Should Be ");
+  Serial.println(0x68, HEX);
+
+  if (c == 0x68) // WHO_AM_I should always be 0x68
+  {
+    Serial.println("MPU6050 is online...");
+
+    mpu.MPU6050SelfTest(SelfTest); // Start by performing self test and reporting values
+    //    Serial.print("x-axis self test: acceleration trim within : "); Serial.print(SelfTest[0],1); Serial.println("% of factory value");
+    //    Serial.print("y-axis self test: acceleration trim within : "); Serial.print(SelfTest[1],1); Serial.println("% of factory value");
+    //    Serial.print("z-axis self test: acceleration trim within : "); Serial.print(SelfTest[2],1); Serial.println("% of factory value");
+    //    Serial.print("x-axis self test: gyration trim within : "); Serial.print(SelfTest[3],1); Serial.println("% of factory value");
+    //    Serial.print("y-axis self test: gyration trim within : "); Serial.print(SelfTest[4],1); Serial.println("% of factory value");
+    //    Serial.print("z-axis self test: gyration trim within : "); Serial.print(SelfTest[5],1); Serial.println("% of factory value");
+
+    if (SelfTest[0] < 1.0f && SelfTest[1] < 1.0f && SelfTest[2] < 1.0f && SelfTest[3] < 1.0f && SelfTest[4] < 1.0f && SelfTest[5] < 1.0f)
+    {
+      Serial.println("Pass Selftest!");
+
+      mpu.calibrateMPU6050(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
+      // Serial.println("MPU6050 bias");
+      // Serial.println(" x\t  y\t  z  ");
+      // Serial.print((int)(1000 * accelBias[0]));
+      // Serial.print('\t');
+      // Serial.print((int)(1000 * accelBias[1]));
+      // Serial.print('\t');
+      // Serial.print((int)(1000 * accelBias[2]));
+      // Serial.println(" mg");
+
+      // Serial.print(gyroBias[0], 1);
+      // Serial.print('\t');
+      // Serial.print(gyroBias[1], 1);
+      // Serial.print('\t');
+      // Serial.print(gyroBias[2], 1);
+      // Serial.println(" o/s");
+
+      mpu.initMPU6050();
+      Serial.println("MPU6050 initialized for active data mode...."); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
+    }
+    else
+    {
+      Serial.print("Could not connect to MPU6050: 0x");
+      Serial.println(c, HEX);
+      while (1)
+        ; // Loop forever if communication doesn't happen
+    }
+  }
+}
+
+void getReadings()
+{
+  // If data ready bit set, all data registers have new data
+  if (mpu.readByte(MPU6050_ADDRESS, INT_STATUS) & 0x01)
+  {                                // check if data ready interrupt
+    mpu.readAccelData(accelCount); // Read the x/y/z adc values
+    aRes = mpu.getAres();
+
+    // Now we'll calculate the accleration value into actual g's
+    ax = (float)accelCount[0] * aRes; // get actual g value, this depends on scale being set
+    ay = (float)accelCount[1] * aRes;
+    az = (float)accelCount[2] * aRes;
+
+    //Serial.print(ax);Serial.print("\t");
+    //Serial.print(ay);Serial.print("\t");
+    //Serial.print(az);Serial.print("\t");
+
+    mpu.readGyroData(gyroCount); // Read the x/y/z adc values
+    gRes = mpu.getGres();
+
+    // Calculate the gyro value into actual degrees per second
+    gx = (float)gyroCount[0] * gRes; // get actual gyro value, this depends on scale being set
+    gy = (float)gyroCount[1] * gRes;
+    gz = (float)gyroCount[2] * gRes;
+
+    //Serial.print(gx);Serial.print("\t");
+    //Serial.print(gy);Serial.print("\t");
+    //Serial.println(gz);
+
+    //tempCount = mpu.readTempData();  // Read the x/y/z adc values
+    //temperature = ((float) tempCount) / 340. + 36.53; // Temperature in degrees Centigrade
+  }
+
+  Now = micros();
+  deltat = ((Now - lastUpdate) / 1000000.0f); // set integration time by time elapsed since last filter update
+  lastUpdate = Now;
+
+  MadgwickQuaternionUpdate(ax, ay, az, gx * PI / 180.0f, gy * PI / 180.0f, gz * PI / 180.0f);
+
+  // Serial print and/or display at 0.5 s rate independent of data rates
+  delt_t = millis() - count;
+  if (delt_t > freq)
+  { // update LCD once per half-second independent of read rate
+    digitalWrite(blinkPin, blinkOn);
+
+    yaw = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);
+    pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
+    roll = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
+
+    pitch *= 180.0f / PI;
+    yaw *= 180.0f / PI;
+    roll *= 180.0f / PI;
+
+    //    Serial.print("average rate = "); Serial.print(1.0f/deltat, 2); Serial.println(" Hz");
+
+    /*Serial.println(" x\t  y\t  z  ");
+
+    Serial.print((int)(1000 * ax)); Serial.print('\t');
+    Serial.print((int)(1000 * ay)); Serial.print('\t');
+    Serial.print((int)(1000 * az));
+    Serial.println(" mg");
+
+    Serial.print((int)(gx)); Serial.print('\t');
+    Serial.print((int)(gy)); Serial.print('\t');
+    Serial.print((int)(gz));
+    Serial.println(" o/s");
+
+    Serial.print((int)(yaw)); Serial.print('\t');
+    Serial.print((int)(pitch)); Serial.print('\t');
+    Serial.print((int)(roll));
+    Serial.println(" ypr");*/
+
+    //Serial.print("rt: "); Serial.print(1.0f / deltat, 2); Serial.println(" Hz");
+
+    blinkOn = ~blinkOn;
+    count = millis();
+  }
+}
